b3d5fa1f49176befd9cf261859fec349a6baa20b
Antonio <marcos_antonio97@live.com>
2020-09-18 09:41:03 -0300

    Arquivos inciais

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using GrpcGreeter;
+using Grpc.Net.Client;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client =  new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "Quitanda da Júlia" });
+            Console.WriteLine("Testando será que entrou aqui? " + reply.Message);
+            Console.WriteLine("Bjo e fica com deus...");
+            Console.ReadKey();
+        }
+    }
+}
